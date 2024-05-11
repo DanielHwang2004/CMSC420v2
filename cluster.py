14c7737d6bb8a6a3f6084adccbc0ff2e3d42c8d7
DanielHwang2004 <hwangdaniel7@gmail.com>
2024-05-10 21:47:32 -0400

    we done?

--- cluster.py
+++ cluster.py
@@ -11,14 +11,17 @@ class Graph():
         # IMPORTANT!!!
         # Replace the next line so the Laplacian is a nodecount x nodecount array of zeros.
         # You will need to do this in order for the code to run!
-        self.laplacian = 1
+        self.laplacian = np.zeros((nodecount, nodecount))
 
     # Add an edge to the Laplacian matrix.
     # An edge is a pair [x,y].
     def addedge(self,edge):
-        # Your code goes here.
-        print('Filler code...')
-        # Nothing to return.
+        first, second = edge
+
+        self.laplacian[first][first] += 1
+        self.laplacian[second][second] += 1
+        self.laplacian[first][second] = -1
+        self.laplacian[second][first] = -1
 
     # Don't change this - no need.
     def laplacianmatrix(self) -> np.array:
@@ -30,7 +33,14 @@ class Graph():
     # If not, negate the whole thing.
     def fiedlervector(self) -> np.array:
         # Replace this next line with your code.
-        fvec = [0]
+        e_vals, e_vecs = np.linalg.eig(self.laplacian)
+        
+        fied_eig = np.argsort(e_vals)[1]
+        
+        fvec = e_vecs[:, fied_eig]
+        
+        if fvec[0] < 0:
+            fvec = -fvec
         # Return
         return fvec
 
@@ -41,7 +51,17 @@ class Graph():
 
     def clustersign(self):
         # Replace the next two lines with your code.
+        
+        fvec = self.fvec()
+        
         pind = []
         nind = []
+        
+        for i, val in enumerate(fvec):
+            if val >= 0:
+                pind.append(i)
+            else:
+                nind.append(i)
+            
         # Return
         return([pind,nind])
